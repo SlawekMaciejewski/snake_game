c19b564739c71ef3c50a7e7726a3838f9a335103
SlawekMaciejewski <slawek74.m@interia.pl>
2021-01-15 20:14:22 +0100

    Creating a new snake as three white segments.

--- main.py
+++ main.py
@@ -4,5 +4,15 @@ from turtle import Turtle, Screen
 if __name__ == '__main__':
     screen = Screen()
     screen.title("SNAKE GAME")
-    screen.setup(width=500, height=500)
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+
+    x = 0
+    for segment in range(3):
+        new_segment = Turtle(shape='square')
+        new_segment.color('white')
+        new_segment.penup()
+        new_segment.goto(x=x, y=0)
+        x += -20
+
     screen.exitonclick()
