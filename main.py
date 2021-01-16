1fee9f1729d08095095ba4d65d99d5ce4718e663
SlawekMaciejewski <slawek74.m@interia.pl>
2021-01-16 17:14:31 +0100

    Added while loop includes animation snake.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from turtle import Turtle, Screen
 
-
 if __name__ == '__main__':
     screen = Screen()
     screen.title("SNAKE GAME")
     screen.setup(width=600, height=600)
     screen.bgcolor("black")
 
+    segments = []
     x = 0
     for segment in range(3):
         new_segment = Turtle(shape='square')
@@ -14,5 +14,11 @@ if __name__ == '__main__':
         new_segment.penup()
         new_segment.goto(x=x, y=0)
         x += -20
+        segments.append(new_segment)
+
+    game_is_on = True
+    while game_is_on:
+        for seg in segments:
+            seg.forward(20)
 
     screen.exitonclick()
