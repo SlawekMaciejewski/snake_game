7323f9606d821f3eb1e150924a54d3f8a8afbf04
SlawekMaciejewski <slawek74.m@interia.pl>
2021-01-16 18:16:21 +0100

    Corrected the comment before for loop

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ if __name__ == '__main__':
     while game_is_on:
         screen.update()
         time.sleep(.05)
-        # The last segment takes the one before the last position, the one before the last segment
-        # takes the next position etc. The first segment always moves forward 20.
+        # The last segment takes the second to last segment, the second to last segment takes the next segment etc.
+        # The first segment always moves forward 20.
         for seg_num in range(len(segments) - 1, 0, -1):
             new_x = segments[seg_num - 1].xcor()
             new_y = segments[seg_num - 1].ycor()
