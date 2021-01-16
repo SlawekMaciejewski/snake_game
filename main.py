d92f318af7cc1bf3dcb02de918f8bae04df83fe2
SlawekMaciejewski <slawek74.m@interia.pl>
2021-01-16 18:07:39 +0100

    The added new idea of moving the snake.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from turtle import Turtle, Screen
+import time
 
 if __name__ == '__main__':
     screen = Screen()
     screen.title("SNAKE GAME")
     screen.setup(width=600, height=600)
     screen.bgcolor("black")
+    screen.tracer(0)
 
     segments = []
     x = 0
@@ -18,7 +20,15 @@ if __name__ == '__main__':
 
     game_is_on = True
     while game_is_on:
-        for seg in segments:
-            seg.forward(20)
+        screen.update()
+        time.sleep(.05)
+        # The last segment takes the one before the last position, the one before the last segment
+        # takes the next position etc. The first segment always moves forward 20.
+        for seg_num in range(len(segments) - 1, 0, -1):
+            new_x = segments[seg_num - 1].xcor()
+            new_y = segments[seg_num - 1].ycor()
+            # time.sleep(1)
+            segments[seg_num].goto(new_x, new_y)
+        segments[0].forward(20)
 
     screen.exitonclick()
