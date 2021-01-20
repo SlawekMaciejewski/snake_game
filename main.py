8c6d09f5482deab22c0581ee15e183c8f9c44c36
SlawekMaciejewski <slawek74.m@interia.pl>
2021-01-20 08:15:27 +0100

    Added a detect collision with the tail, if a head collides with any segment in the tail. Game is completed.

--- main.py
+++ main.py
@@ -34,9 +34,16 @@ if __name__ == '__main__':
             food.refresh()
             snake.extend()
             scoreboard.increase_score()
-        # Detect collision with wall
+        # Detect collision with a wall
         if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
             game_is_on = False
             scoreboard.game_over()
+        # Detect collision with a tail
+        # if head collides with any segment in the tail
+        # The head is the zero segment, so we need to leave it out. We are looping through a for loop without the head.
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 10:
+                game_is_on = False
+                scoreboard.game_over()
 
     screen.exitonclick()
