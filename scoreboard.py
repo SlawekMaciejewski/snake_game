6b033102308c6792c3f3ad2db8706adeb67093d3
SlawekMaciejewski <slawek74.m@interia.pl>
2021-02-05 08:20:12 +0100

    High score added to write and read from the file.

--- scoreboard.py
+++ scoreboard.py
@@ -1,8 +1,12 @@
 from turtle import Turtle
 
-
 ALIGNMENT = "center"
 FONT = ("Courier", 18, "normal")
+
+
+def read_file():
+    with open("score.txt") as f:
+        return int(f.read())
 
 
 class Scoreboard(Turtle):
@@ -10,7 +14,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = read_file()
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -21,18 +25,21 @@ class Scoreboard(Turtle):
         self.clear()
         self.write(f"SCORE : {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def reset(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
-        self.score = 0
-        self.update_scoreboard()
-
     # def game_over(self):
     #     self.home()
     #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.write_file()
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
 
-
+    def write_file(self):
+        with open("score.txt", "w") as f:
+            f.write(str(self.high_score))
